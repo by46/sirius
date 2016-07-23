2ba22ed41388e31da89f103775448b5c105cc458
jaydon.t.jin <jaydon.t.jin@newegg.com>
2016-07-23 11:07:26 +0800

    add function

--- sirius/docker.py
+++ sirius/docker.py
@@ -82,6 +82,5 @@ def build_image():
     :return:
     """
     from fabric.api import local
-    
     local('docker run --rm -v ${WORKSPACE}:/home/matrix -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock docker.neg/matrix:0.0.3 /usr/local/bin/matrix.sh')
 
