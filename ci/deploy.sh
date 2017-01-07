3f7f507520136501802aaaf1553696f1cdecc4f4
by46 <ycs_ctbu_2010@126.com>
2017-01-07 09:11:58 +0800

    fix: add exit code check for deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,6 +2,12 @@
 
 python setup.py sdist upload -r http://scmesos06
 
+# release failed
+[ $? -gt 0 ] && exit 1
+
 pip uninstall -y sirius
 
 pip --trusted-host scmesos06 install -i http://scmesos06/simple sirius
+
+# install failed
+[ $? -gt 0 ] && exit 2
